04ba5b76d814c7f8ebc1b6acb417a24cd824dca2
Noam Revivo <noam.revivo.1@gmail.com>
2025-12-07 13:15:04 +0200

    feat: update chat API with Zod validation and add zod dependency

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { OpenAI } from 'openai';
+import { Conversations } from 'openai/resources';
+import z from 'zod';
 dotenv.config();
 
 const client = new OpenAI({
@@ -20,15 +22,38 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
 
+let lastResponseId: string | null = null;
+// conversationId -> lastResponseId
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'prompt is required')
+      .max(1000, 'prompt is too long, max 1000 characters'),
+   conversationId: z.string().uuid(),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+   }
+
+   const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
    });
+
+   conversations.set(conversationId, response.id);
+
    res.json({ message: response.output_text });
 });
 

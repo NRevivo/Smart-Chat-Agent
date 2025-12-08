6547af24654a6fd36e8b07dfca268e1614ac27bb
Noam Revivo <noam.revivo.1@gmail.com>
2025-12-08 18:03:08 +0200

    fixing small issue

--- packages/client/src/components/ui/chat/ChatBot.tsx
+++ packages/client/src/components/ui/chat/ChatBot.tsx
@@ -4,14 +4,6 @@ import ChatInput, { type ChatFormData } from './ChatInput';
 import type { Message } from './ChatMessages';
 import ChatMessages from './ChatMessages';
 import TypingIndicator from './TypingIndicator';
-import popSound from '@/assets/sounds/pop.mp3';
-import notificationSound from '@/assets/sounds/notification.mp3';
-
-const popAudio = new Audio(popSound);
-popAudio.volume = 0.2;
-
-const notificationAudio = new Audio(notificationSound);
-notificationAudio.volume = 0.2;
 
 type ChatResponse = {
    message: string;
@@ -28,7 +20,6 @@ const ChatBot = () => {
          setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
          setIsBotTyping(true);
          setError('');
-         popAudio.play();
 
          const { data } = await axios.post<ChatResponse>('/api/chat', {
             prompt,
@@ -38,7 +29,6 @@ const ChatBot = () => {
             ...prev,
             { content: data.message, role: 'bot' },
          ]);
-         notificationAudio.play();
       } catch (error) {
          console.error(error);
          setError('Something went wrong, try again!');
